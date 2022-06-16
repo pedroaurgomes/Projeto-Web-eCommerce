ee419c02d32086f3ae944610b7e8797b79e0475a
GabrielDertoni <gab.dertoni@gmail.com>
2022-06-16 00:17:18 -0300

    Adiciona algumas funcoes para simular o backend

---
+++ vintage-store-client/src/mock/products.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 0,
+        title: "T-shirt",
+        brand: "Some Brand",
+        price: 50.99,
+        description: "Really good!",
+        colors: ["red", "green", "blue"],
+        defaultColor: 0,
+    },
+    {
+        id: 1,
+        title: "T-shirt",
+        brand: "Some Brand",
+        price: 50.99,
+        description: "Really good!",
+        colors: ["red", "green", "blue"],
+        defaultColor: 0,
+    },
+    {
+        id: 2,
+        title: "T-shirt",
+        brand: "Some Brand",
+        price: 50.99,
+        description: "Really good!",
+        colors: ["red", "green", "blue"],
+        defaultColor: 0,
+    },
+    {
+        id: 3,
+        title: "T-shirt",
+        brand: "Some Brand",
+        price: 50.99,
+        description: "Really good!",
+        colors: ["red", "green", "blue"],
+        defaultColor: 0,
+    }
+]
