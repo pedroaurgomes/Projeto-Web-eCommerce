591c7dfd101fe24ddfb2e7ece371f501cc4764e0
GabrielDertoni <gab.dertoni@gmail.com>
2022-06-18 19:16:10 -0300

    Adiciona numero de produtos vendidos e em estoque

--- vintage-store-client/src/mock/products.js
+++ vintage-store-client/src/mock/products.js
@@ -9,6 +9,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/sweatshirt1.png",
         longDescription: "Malha preta com estampa nas costas, se vê escrito em rosa e em inglês “Emo till Death”, em português “Emo até a morte”. Embaixo se vê um crânio com franja preta caída sobre a região dos olhos, envolto de rosas vermelhas.",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 1,
@@ -20,6 +22,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/sweatshirt2.png",
         longDescription: "Malha preta com estampa na frente, se vê uma manete de vídeo games cinza com detalhes vermelhos e laranjas.",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 2,
@@ -31,6 +35,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/sweatshirt3.png",
         longDescription: "Malha preta com estampa na frente, se vê um crânio com capacete de samurai, com duas katanas (espadas samurais) cruzadas atrás. Acima está escrito em vermelho e em inglês “Undead”, em português “Morto-vivo”.",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 3,
@@ -42,6 +48,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/sweatshirt4.png",
         longDescription: "Malha preta, com forro e sobreposição nas mangas lilás, com estampa na frente, se vê uma menina com luvas, uma camiseta cropped e uma saia lilás, com cabelo longo e alaranjado. Atrás dela se vê uma meia lua na horizontal.",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 4,
@@ -53,6 +61,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/t-shirt1.png",
         longDescription: "Sabe aquela sua camiseta preferida que você usa até não dar mais?! A Reserva também tem os seus xodós da temporada e uma delas com certeza é a sua cara!",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 5,
@@ -64,6 +74,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/t-shirt2.png",
         longDescription: "Linha: Better Cotton Initiative - Os nossos produtos de algodão apoiam a produção de algodão sustentável. Isto faz parte da nossa ambição de acabar com o desperdício de plásticos.",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 6,
@@ -75,6 +87,8 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/t-shirt3.png",
         longDescription: "A malha é um tecido leve e flexível, resultado da tricotagem do tecido. Extremamente confortável, macia ao toque e versátil, é a queridinha para confecção de camisetas, blusas e vestidos, além de ter uma alta durabilidade, perfeita para compor um guarda-roupa minimalista e duradouro.",
+        nSold: 0,
+        nInStock: 10,
     },
     {
         id: 7,
@@ -86,5 +100,7 @@ export default [
         defaultColor: 0,
         imgSrc: "/assets/products/t-shirt4.png",
         longDescription: "As camisetas são itens indispensáveis em um guarda-roupa moderno! Elas são extremamente curingas, combinam com diversas composições e são perfeitas para dar aquele toque urbano e descolado com personalidade! Aposte com peças leves para um look esportivo ou peças mais estruturadas para um visual que vai desde o trabalho ao happy hour!",
+        nSold: 0,
+        nInStock: 10,
     },
 ]
