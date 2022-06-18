9f103a75ad14bc33b0601f956f56a0b2c4cffb96
Gabriel Dertoni <gab.dertoni@gmail.com>
2022-06-17 22:13:06 -0300

    Adiciona pagina 'Minha Conta'

---
+++ vintage-store-client/src/views/AccountView.vue
@@ -0,0 +1,107 @@
+<template>
+  <main class="main-column">
+    <h2>Minha Conta</h2>
+    <LoadingModal v-if="isLoading"></LoadingModal>
+    <div class="grid-2-cols">
+      <div class="flex-col gap-sm">
+        <TextField
+          label="name"
+          v-model="name"
+          placeholder="..."
+        />
+        <TextField
+          label="email"
+          v-model="email"
+          placeholder="..."
+        />
+        <TextField
+          label="address"
+          v-model="address"
+          placeholder="..."
+        />
+        <div class="flex-row flex-space-between">
+          <TextField
+            label="city"
+            v-model="city"
+            placeholder="..."
+          />
+          <TextField
+            label="extra"
+            v-model="extra"
+            placeholder="..."
+          />
+        </div>
+        <TextField
+          label="phone"
+          v-model="phone"
+          placeholder="..."
+        />
+        <div class="flex-row flex-space-between">
+          <Button color="blue" @click="update">Salvar</Button>
+          <Button color="red" @click="logout">Sair</Button>
+        </div>
+      </div>
+      <div>
+        /* Imagem do sene */
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+import { trueWhileAction } from "@/utils";
+
+import TextField from "@/components/TextField.vue";
+import Button from "@/components/Button.vue";
+import LoadingModal from "@/components/LoadingModal.vue";
+
+export default {
+  components: {
+    TextField,
+    Button,
+    LoadingModal,
+  },
+  data: () => ({
+    name: "",
+    email: "",
+    address: "",
+    city: "",
+    extra: "",
+    phone: "",
+
+    // TODO: make some visual loading bar.
+    isLoading: false,
+  }),
+  created() {
+    const user = this.$store.getters.user
+    this.name = user.name;
+    this.email = user.email;
+    this.address = user.address;
+    this.city = user.city;
+    this.extra = user.extra;
+    this.phone = user.phone;
+  },
+  methods: {
+    update() {
+      // TODO: Data validation.
+      trueWhileAction(
+        b => this.isLoading = b,
+        this.$store.dispatch("updateUser", {
+          name: this.name,
+          email: this.email,
+          address: this.address,
+          city: this.city,
+          extra: this.extra,
+          phone: this.phone
+        })
+      )
+    },
+    async logout() {
+      await trueWhileAction(
+        b => this.isLoading = b,
+        this.$store.dispatch("logout")
+      );
+      this.$router.replace({ name: "home" });
+    }
+  }
+}
+</script>
