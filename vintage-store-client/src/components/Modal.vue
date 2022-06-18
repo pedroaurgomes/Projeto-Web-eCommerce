df7b2315b92de137f5ca8b2bc20461b8db650df8
Gabriel Dertoni <gab.dertoni@gmail.com>
2022-06-17 22:12:16 -0300

    Adiciona modais

---
+++ vintage-store-client/src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="modal-container" @click="clickOutside">
+        <div class="modal" @click.stop="" :style="style">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundColor: {
+      type: String,
+      required: false,
+      default: "white",
+    }
+  },
+  computed: {
+    style() {
+      return {
+        backgroundColor: this.backgroundColor,
+      };
+    }
+  },
+  methods: {
+    clickOutside() {
+      this.$emit("clickOutside")
+    }
+  }
+}
+</script>
+
+<style>
+.modal-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+.modal {
+    border-radius: 10px;
+    padding: 1em;
+}
+</style>
---
+++ vintage-store-client/src/components/RegisterModal.vue
@@ -0,0 +1,116 @@
+<template>
+  <Modal>
+    <h2>Cadastrar</h2>
+
+    <div class="register-input-col">
+      <TextField
+        label="name"
+        v-model="name"
+        placeholder="Nome"
+      />
+      <TextField
+        label="id"
+        v-model="id"
+        placeholder="CPF"
+      />
+      <TextField
+        label="email"
+        v-model="email"
+        placeholder="Email"
+      />
+      <TextField
+        label="phone"
+        v-model="phone"
+        placeholder="Telefone"
+      />
+      <TextField
+        label="address"
+        v-model="address"
+        placeholder="Endereço"
+      />
+      <div class="flex-row flex-space-between">
+        <TextField
+          label="city"
+          v-model="city"
+          placeholder="Cidade"
+        />
+        <TextField
+          label="extra"
+          v-model="extra"
+          placeholder="Complemento"
+        />
+      </div>
+      <TextField
+        label="password"
+        v-model="password"
+        placeholder="Senha"
+      />
+    </div>
+    <br>
+    <Button @click="submit">Cadastrar</Button>
+  </Modal>
+</template>
+<script>
+import TextField from "@/components/TextField.vue";
+import Button from "@/components/Button.vue";
+import Modal from "@/components/Modal.vue";
+
+export default {
+  components: {
+    TextField,
+    Button,
+    Modal,
+  },
+  data: () => ({
+    name: "",
+    id: "",
+    email: "",
+    phone: "",
+    address: "",
+    city: "",
+    extra: "",
+    password: "",
+    isLoading: false,
+  }),
+  methods: {
+    async submit() {
+      // TODO: data validation
+      this.isLoading = true;
+      await this.sendRegistration();
+      this.isLoading = false;
+    },
+    async sendRegistration() {
+      const res = await this.$store.dispatch("registerUser", {
+        name: this.name,
+        id: this.id,
+        email: this.email,
+        phone: this.phone,
+        address: this.address,
+        city: this.city,
+        extra: this.extra,
+        password: this.password,
+        type: "customer",
+      });
+
+      if (res instanceof Error) {
+        alert(`Erro na registração: ${res.message}`);
+        return;
+      }
+
+      // Go back to the previous page
+      this.$router.replace({ name: "account" });
+    }
+  }
+}
+</script>
+<style>
+.register-input-col {
+  display: flex;
+  flex-direction: column;
+  gap: .8em;
+}
+
+.register-input-col > * {
+  width: 100%;
+}
+</style>
