c0c688c98f230635bd093cc81e7aafbedb03beba
JoaoVSene <joaovictor_sene@hotmail.com>
2022-07-14 17:20:36 -0300

    Update product

--- vintage-store-client/src/store/modules/products.js
+++ vintage-store-client/src/store/modules/products.js
@@ -66,7 +66,25 @@ export default {
       return product;
     },
     async updateProduct({ commit }, product) {
-      const res = await mock.updateProduct(product);
+      // const res = await mock.updateProduct(product);
+
+      const res = await fetch(`http://localhost:8080/api/user/${state.id}`, { 
+        method: "PATCH",
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(product),
+      }).then(o => o.json());
+      
+      console.log("Res: ")
+      console.log(res);
+      console.log(product);
+
+      if (!res.ok) return new Error(res.error);
+      commit("register", user);
+      return res.user;
+
+
       if (!res.ok) return new Error(res.error);
       commit("addProductToCache", res.product);
       return res.product;
